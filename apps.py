4e7208a5b1fbbec5c975a313fc119774849ee35f
Arati778 <133510777+Arati778@users.noreply.github.com>
2023-05-15 08:18:52 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OnlineTestingSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Online_Testing_System'
---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from Online_Testing_System.views import *
+app_name='Online_Testing_System'
+urlpatterns = [
+    path('',welcome),
+    path('new-candidate',candidateRegistrationForm,name='registrationForm'),
+    path('store-candidate',candidateRegistration,name='storeCandidate'),
+    path('login',loginView,name='login'),
+    path('home',candidateHome,name='home'),
+    path('test-paper',testPaper,name='testPaper'),
+    path('calculate-result',calculateTestResult,name='calculateTest'),
+    path('test-history',testResultHistory,name='testHistory'),
+    path('result',showTestResult,name='result'),
+    path('logout',logoutView,name='logout'),
+]
+
+
